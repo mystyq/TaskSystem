64fbe1f7a2d88bbb11d3fb062e8223bc31dcd2c4
jmayoral <joseweig@gmail.com>
2023-12-29 23:08:24 -0600

    - Added priority and comment fields to Task model - Added priority select and comment input to create form - Added priority column to tasks table - Added Priority enum - Added migration

---
+++ Migrations/20231230041031_AddPriorityAndCommentFields.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPriorityAndCommentFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Comment",
+                table: "Tasks",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Priority",
+                table: "Tasks",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Comment",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "Tasks");
+        }
+    }
+}
