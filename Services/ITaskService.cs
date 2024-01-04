63511e1ca552519723bb5f99c24fc46cf213f8d1
jmayoral <joseweig@gmail.com>
2024-01-04 12:42:08 -0600

    Created TaskService interface and implementation

---
+++ Services/ITaskService.cs
@@ -0,0 +1,17 @@
+﻿namespace TaskSystem.Services
+{
+    public interface ITaskService
+    {
+        Task<List<Data.Models.Task>> GetAllTasksAsync();
+
+        Task<Data.Models.Task> GetTaskByIdAsync(int id);
+
+        Task AddTaskAsync(Data.Models.Task task);
+
+        Task UpdateTaskAsync(Data.Models.Task task);
+
+        Task DeleteTaskAsync(int id);
+
+        Task DeleteAllTasksAsync();
+    }
+}
---
+++ Services/TaskService.cs
@@ -0,0 +1,60 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using TaskSystem.Data;
+
+namespace TaskSystem.Services
+{
+    public class TaskService : ITaskService
+    {
+        private readonly DatabaseContext _context;
+
+        public TaskService(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task DeleteAllTasksAsync()
+        {
+            var tasks = await _context.Tasks.ToListAsync();
+            _context.Tasks.RemoveRange(tasks);
+            await _context.SaveChangesAsync();
+
+            //await _context.Database.ExecuteSqlRaw("DELETE FROM Tasks");
+            //await _context.Database.ExecuteSqlRaw("DELETE FROM SQLITE_SEQUENCE WHERE name='Tasks'");
+        }
+
+        public async Task DeleteTaskAsync(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            if (task is not null)
+            {
+                _context.Tasks.Remove(task);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<List<Data.Models.Task>> GetAllTasksAsync()
+        {
+            var tasks = await _context.Tasks.ToListAsync();
+            return tasks;
+        }
+
+        public async Task<Data.Models.Task> GetTaskByIdAsync(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            return task;
+        }
+
+        public async Task AddTaskAsync(Data.Models.Task task)
+        {
+            _context.Tasks.Add(task);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateTaskAsync(Data.Models.Task task)
+        {
+            _context.Tasks.Update(task);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
