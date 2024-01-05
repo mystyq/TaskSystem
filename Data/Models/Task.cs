6e582fe44e8dbb6857bd8e0ad5236e2368735d79
mystyq <jrlamping@gmail.com>
2024-01-05 10:50:19 -0600

    Move Task Status validation to model

--- Data/Models/Task.cs
+++ Data/Models/Task.cs
@@ -8,7 +8,16 @@ namespace TaskSystem.Data.Models
         public string? Title { get; set; }
         public string? Description { get; set; }
         public DateTime? DateCreated { get; set; }
-        public Status? Status { get; set; } = Enums.Status.Staged;
+        private Status? _status = Enums.Status.Staged;
+        public Status? Status
+        {
+            get => _status;
+            set
+            {
+                if (_status < value)
+                    _status = value;
+            }
+        }
         public Priority? Priority { get; set; } = Enums.Priority.None;
         public string? Comment { get; set; }
         public bool? Archived { get; set; } = false;
